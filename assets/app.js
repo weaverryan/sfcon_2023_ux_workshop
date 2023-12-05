fef6b9ac0ccc5a7d9a2031c3904bdf9c247acd44
Ryan Weaver <ryan@symfonycasts.com>
2023-12-05 11:35:14 -0500

    Step 6: Modules: Move greeting to app.js

--- assets/app.js
+++ assets/app.js
@@ -5,5 +5,7 @@
  * which should already be in your base.html.twig.
  */
 import './styles/app.css'
+import greet from './lib/greeting.js';
 
+greet('Ryan');
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉')
