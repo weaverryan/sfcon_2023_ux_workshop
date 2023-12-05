a4bc5a4ccc6f2002b82c0d1589240f2780484648
Ryan Weaver <ryan@symfonycasts.com>
2023-12-05 11:35:21 -0500

    Step 41: Toast: close transition

--- assets/controllers/closeable_controller.js
+++ assets/controllers/closeable_controller.js
@@ -1,7 +1,17 @@
 import { Controller } from '@hotwired/stimulus';
+import { useTransition } from 'stimulus-use';
 
 export default class extends Controller {
+    connect() {
+        useTransition(this, {
+            leaveActive: 'transition ease-in duration-200',
+            leaveFrom: 'opacity-100',
+            leaveTo: 'opacity-0',
+            transitioned: true,
+        })
+    }
+
     close() {
-        this.element.remove();
+        this.leave();
     }
 }
