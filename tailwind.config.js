aa8a758cdf8115bb172a6c6bee83c775cfb6347c
Ryan Weaver <ryan@symfonycasts.com>
2023-12-05 11:35:16 -0500

    Step 16: Tailwind: Finish bundle setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     "./assets/**/*.js",
     "./templates/**/*.html.twig",
+    "./vendor/tales-from-a-dev/flowbite-bundle/templates/**/*.html.twig",
   ],
   theme: {
     extend: {},
