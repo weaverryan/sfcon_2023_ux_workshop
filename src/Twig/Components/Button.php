19459142b7ae0eb2e4abb915f6852aae0c9d4fa4
Ryan Weaver <ryan@symfonycasts.com>
2023-12-06 11:36:09 -0500

    Step 50: TwigComponent: variantClasses

--- src/Twig/Components/Button.php
+++ src/Twig/Components/Button.php
@@ -8,4 +8,15 @@ use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 class Button
 {
     public string $variant = 'default';
+
+    public function variantClasses(): string
+    {
+        return match ($this->variant) {
+            'default' => 'text-white bg-blue-500 hover:bg-blue-700',
+            'success' => 'text-white bg-green-600 hover:bg-green-700',
+            'danger' => 'text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:ring-red-300 focus:outline-none',
+            'link' => 'text-white',
+            default => throw new \LogicException(sprintf('Unknown button type "%s"', $this->variant)),
+        };
+    }
 }
