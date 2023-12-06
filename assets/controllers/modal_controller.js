f89c58d78bbc47a766e530cbf63cad39393fffb9
Ryan Weaver <ryan@symfonycasts.com>
2023-12-06 11:36:12 -0500

    Step 66: AjaxModal: Add observer to controller

--- assets/controllers/modal_controller.js
+++ assets/controllers/modal_controller.js
@@ -1,7 +1,39 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-    static targets = ['dialog'];
+    static targets = ['dialog', 'content'];
+
+    observer = null;
+
+    connect() {
+        if (this.hasContentTarget) {
+            // create a mutation observer on this.modalContentTarget
+            // when the content changes, call this.open()
+            this.observer = new MutationObserver(() => {
+                const shouldOpen = this.contentTarget.innerHTML.trim().length > 0;
+
+                if (shouldOpen && !this.dialogTarget.open) {
+                    this.open();
+                } else if (!shouldOpen && this.dialogTarget.open) {
+                    this.close();
+                }
+            });
+            this.observer.observe(this.contentTarget, {
+                childList: true,
+                characterData: true,
+                subtree: true
+            });
+        }
+    }
+
+    disconnect() {
+        if (this.observer) {
+            this.observer.disconnect();
+        }
+        if (this.dialogTarget.open) {
+            this.close();
+        }
+    }
 
     open() {
         this.dialogTarget.showModal();
